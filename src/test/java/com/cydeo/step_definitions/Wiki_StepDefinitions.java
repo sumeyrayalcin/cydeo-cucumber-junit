1e92ce34862fcc4da5313b591f692e4e7722df66
Sumeyra <smyryalcin@gmail.com>
2023-02-28 11:14:03 +0000

    wikiSearch.feature Wiki_StepDefinitions

---
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -0,0 +1,27 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Wiki_StepDefinitions {
+    @Given("User is on Wikipedia home page")
+    public void user_is_on_wikipedia_home_page() {
+        Driver.getDriver().get("https://www.wikipedia.org");
+    }
+    @When("User types {string} in the wiki search box")
+    public void user_types_in_the_wiki_search_box(String string) {
+
+    }
+    @When("User clicks wiki search button")
+    public void user_clicks_wiki_search_button() {
+
+    }
+    @Then("User sees {string} is in the wiki title")
+    public void user_sees_is_in_the_wiki_title(String string) {
+
+    }
+
+
+}
