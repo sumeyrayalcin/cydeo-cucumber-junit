82d49f3043c4e0b8e2cfd00aec029438a59bcaed
Sumeyra <smyryalcin@gmail.com>
2023-02-28 15:00:57 +0000

    day 17 Dropdowns page

--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
@@ -1,7 +1,10 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
+import javax.swing.*;
 import java.util.List;
 
 public class DataTable_StepDefinitions {
@@ -13,4 +16,14 @@ public class DataTable_StepDefinitions {
         System.out.println(fruits.get(1));
     }
 
+    @Given("User is on the dropdowns page of practice tool")
+    public void userIsOnTheDropdownsPageOfPracticeTool() {
+        Driver.getDriver().get("https://practice.cydeo.com/dropdown");
+
+    }
+
+    @Then("User should see below info in month dropdown")
+    public void userShouldSeeBelowInfoInMonthDropdown(List<String> expectedMonths) {
+        System.out.println("expectedMonths = " + expectedMonths);
+    }
 }
