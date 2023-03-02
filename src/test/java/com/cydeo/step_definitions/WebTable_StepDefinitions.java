454ad9284e410252a01c37ff2e7aa53587f9eef9
Sumeyra <smyryalcin@gmail.com>
2023-03-02 12:45:17 +0000

    day 17 Web table user order feature

--- src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
@@ -8,6 +8,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.Map;
+
 public class WebTable_StepDefinitions {
 
     WebTableLoginPage webTableLoginPage =new WebTableLoginPage();
@@ -43,5 +45,16 @@ public class WebTable_StepDefinitions {
     @When("user enters username {string} password {string} and logins")
     public void userEntersUsernamePasswordAndLogins(String username, String pw) {
         webTableLoginPage.login(username,pw);
+    }
+
+    @When("User enters below credentials")
+    public void user_enters_below_credentials(Map<String, String> credentials) {
+        //        webTableLoginPage.inputUsername.sendKeys(credentials.get("username"));
+//        webTableLoginPage.inputPassword.sendKeys(credentials.get("password"));
+//        webTableLoginPage.loginButton.click();
+
+        //we can call our login utility method and pass values from map
+        webTableLoginPage.login(credentials.get("username"), credentials.get("password"));
+
     }
 }
--- src/test/resources/features/webTableLogin.feature
+++ src/test/resources/features/webTableLogin.feature
@@ -1,5 +1,7 @@
 Feature: User should be able to login using correct credentials
 
+  Background: user is on the login page
+    Given user is on the login page of web table app
 
   Scenario: Positive login scenario
     Given user is on the login page of web table app
@@ -15,4 +17,13 @@ Feature: User should be able to login using correct credentials
     Then user should see url contains orders
 
     #1- implement this new step
-    #2-
+
+
+  Scenario: User should be able to see all 12 months in
+  months
+  dropdown
+    Given user is on the login page of web table app
+    When User enters below credentials
+      | username     | Test   |
+      | password     | Tester |
+    Then user should see url contains orders
